0155ba613efa33f89a02eeabc4f99256c6fc8c40
nanyunhe <58999282+nanyunhe@users.noreply.github.com>
2022-03-31 19:09:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,11 +11,10 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defau
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git adh https://github.com/Aibx/luci-app-adguardhome' feeds.conf.default
-sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
+
